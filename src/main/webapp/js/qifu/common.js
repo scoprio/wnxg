dc6f7ad1ef9a111691da51049203c9bc296d6a20
peng.wang <peng.wang@21chinamall.com>
2017-08-29 16:31:37 +0800

    #  样式31

--- src/main/webapp/js/qifu/common.js
+++ src/main/webapp/js/qifu/common.js
@@ -4,23 +4,22 @@ function layer_tip(form_tip,fun){
       var layer_box = $('<div class = "layer_box"></div>');
           $('body').append(layer_box);
           $('.layer_box').css({
-              "font-size":'14px',
-              "color":"#fff",
-              "position":"fixed",
-              'left':'0',
-              'right':'0',
-              'top':'0',
-              'bottom':'0',
-              'margin':'auto',
-              'width':'180px',
-              'height':'45px',
-              'padding':'0 5px',
-              'text-align':'center',
-              'background':'rgba(0,0,0,0.5)',
-              'line-height':'45px',
-              'letter-spacing':'1px',
-              'border-radius':'4px',
-              'z-index':'10000'
+                  "font-size":'14px',
+                  "color":"#fff",
+                  "position":"fixed",
+                  'left':'0',
+                  'right':'0',
+                  'top':'25%',
+                  'margin':'auto',
+                  'width':'180px',
+                  'height':'auto',
+                  'padding':'10px 15px',
+                  'text-align':'center',
+                  'background':'rgba(0,0,0,0.5)',
+                  'line-height':'22px',
+                  'letter-spacing':'1px',
+                  'border-radius':'4px',
+                  'z-index':'100000'
 
           });
           $('.layer_box').text(form_tip);
